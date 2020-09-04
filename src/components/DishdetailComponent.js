5b124bc825948b5d0a54b65c2769b9f9b373c3a4
unknown <jeevith15@gmail.com>
2020-09-04 20:30:56 +0530

    spa part2

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,6 +1,6 @@
 import React  from "react";
-import {Card, CardImg, CardText,CardBody,CardTitle} from 'reactstrap';
-
+import {Card, CardImg, CardText,CardBody,CardTitle,Breadcrumb,BreadcrumbItem} from 'reactstrap';
+import { Link} from "react-router-dom";
 
 
 
@@ -53,13 +53,23 @@ import {Card, CardImg, CardText,CardBody,CardTitle} from 'reactstrap';
             return (<div></div>)
         }
         const dishID = <RenderDish dish={props.dish}/>
-        const comments= <RenderComments comments={props.dish.comments}/>        
+        const comments= <RenderComments comments={props.comments}/>        
         return(
             <div className="container">
                 <div className="row">
-                    {dishID}
-                    {comments}
+                    <Breadcrumb>
+                        <BreadcrumbItem><Link to="/menu">Menu</Link> </BreadcrumbItem>
+                        <BreadcrumbItem active>{props.dish.name} </BreadcrumbItem>
+                    </Breadcrumb>
+                    <div className="col-12">
+                        <h3>{props.dish.name}</h3>
+                        <hr/>
+                    </div>
                 </div>
+                    <div className="row">
+                        {dishID}
+                        {comments}
+                    </div>
             </div>
         );
     };
